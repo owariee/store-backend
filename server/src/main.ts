1c4892a00e805428ac30edfb1bf52233e186c3ef
owarie <user962@yahoo.com>
2021-11-02 01:27:39 -0300

    Babel removed, Typescript enter

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import { MongoClient } from 'mongodb';
 
-dotenv.config();
+// dotenv.config();
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/';
 const dbName = process.env.DB_NAME || 'store';
--- server/src/middleware/DBDelete.ts
+++ server/src/middleware/DBDelete.ts
@@ -16,4 +16,3 @@ function DBDelete(collection) {
 }
 
 export default DBDelete;
-
